e3f2825ffc59d45a0184bcf7689a235c5d7e4323
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-02-13 15:23:48 -0800

    worked on the styling of About Me page"

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import '../stylesheets/AboutMe.css';
+import '../stylesheets/AboutMe.css';
 
 class AboutMe extends React.Component {
     render() {
@@ -7,10 +7,9 @@ class AboutMe extends React.Component {
             <div className="main-container">
                 <div className="left-container"></div>
                 <div className="right-container">
-                    <h1 className="about-me-title">About me</h1>
-                    <p className="about-me-text"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea nihil nisi consequatur unde officiis libero, dolores sequi modi. Maiores temporibus repellat fuga atque rem ipsa molestiae distinctio est? Suscipit, consequatur?</p>
+                    <h1 className="title-aboutme">About me</h1>
+                    <p className="text-aboutme"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea nihil nisi consequatur unde officiis libero, dolores sequi modi. Maiores temporibus repellat fuga atque rem ipsa molestiae distinctio est? Suscipit, consequatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt est impedit, pariatur assumenda nam soluta non doloribus consectetur optio odio sapiente, minima tenetur at natus dolorum laboriosam? Fugit, molestias recusandae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam cupiditate ea officia, deserunt voluptatibus aliquid, sed necessitatibus perferendis similique natus totam quidem nam? Recusandae dignissimos veritatis, architecto sit ex praesentium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ex, eum voluptatem iusto error est alias atque accusamus, perferendis fugit porro laboriosam expedita illo molestiae asperiores fuga corrupti culpa explicabo!</p>
                 </div>
-                <div className="home-button">Home</div>
             </div>
         );
     };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import Landing from './components/Landing';
-import Home from './components/Home';
-// import AboutMe from './components/AboutMe';
+// import Home from './components/Home';
+import AboutMe from './components/AboutMe';
 // import Blog from './components/Blog';
 
 import './default.css';
 
 
-ReactDOM.render(<Home/>, document.getElementById('root'));
+ReactDOM.render(<AboutMe/>, document.getElementById('root'));
 
