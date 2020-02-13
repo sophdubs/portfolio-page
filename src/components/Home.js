d3bde8a3befebeaa80fd7f8681ca0e8f248c4191
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-02-12 18:27:14 -0800

    Updated all the CSS classes in Home.js and Home.css to take into consideration the default styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,45 @@
 import React from 'react';
 
-// import '../stylesheets/Home.css';
+import '../stylesheets/Home.css';
 
 class Home extends React.Component {
     render() {
         return (
-            <div className="page">
-                <div className="left-container">
-                    <h1 className="title">Hello World</h1>
-                    <div className="bee-image"></div>
+            <div className="main-container">
+                <div className="left-container left-home">
+                    <h1 className="title-home">Hello World</h1>
+                    <div className="bee-image-home"></div>
                 </div>
-                <div className="right-container">
-                    <div className="first-row">
-                        <div className="hex one"></div>
-                        <div className="hex two link link-one"><p>Skills</p></div>
-                        <div className="hex three"></div>
-                        <div className="hex four"></div>
+                <div className="right-container right-home">
+                    <div className="first-row-home">
+                        <div className="hex-home one-home"></div>
+                        <div className="hex-home two-home link-home link-one-home"><p>Skills</p></div>
+                        <div className="hex-home three-home"></div>
+                        <div className="hex-home four-home"></div>
                     </div>
-                    <div className="second-row">
-                        <div className="hex five link link-two"><p>About Me</p></div>
-                        <div className="hex six"></div>
-                        <div className="hex seven"></div>
-                        <div className="hex eight"></div>
+                    <div className="second-row-home">
+                        <div className="hex-home five-home link-home link-two-home"><p>About Me</p></div>
+                        <div className="hex-home six-home"></div>
+                        <div className="hex-home seven-home"></div>
+                        <div className="hex-home eight-home"></div>
                     </div>
-                    <div className="third-row">
-                        <div className="hex nine"></div>
-                        <div className="hex ten"></div>
-                        <div className="hex eleven link link-three"><p>Projects</p></div>
-                        <div className="hex twelve"></div>
+                    <div className="third-row-home">
+                        <div className="hex-home nine-home"></div>
+                        <div className="hex-home ten-home"></div>
+                        <div className="hex-home eleven-home link-home link-three-home"><p>Projects</p></div>
+                        <div className="hex-home twelve-home"></div>
                     </div>
-                    <div className="fourth-row">
-                        <div className="hex thirteen"></div>
-                        <div className="hex fourteen link link-four"><p>Blog</p></div>
-                        <div className="hex fifteen"></div>
-                        <div className="hex sixteen"></div>
+                    <div className="fourth-row-home">
+                        <div className="hex-home thirteen-home"></div>
+                        <div className="hex-home fourteen-home link-home link-four-home"><p>Blog</p></div>
+                        <div className="hex-home fifteen-home"></div>
+                        <div className="hex-home sixteen-home"></div>
                     </div>
-                    <div className="fifth-row">
-                        <div className="hex seventeen"></div>
-                        <div className="hex eighteen"></div>
-                        <div className="hex nineteen"></div>
-                        <div className="hex twenty link link-five"><p>Contact</p></div>
+                    <div className="fifth-row-home">
+                        <div className="hex-home seventeen-home"></div>
+                        <div className="hex-home eighteen-home"></div>
+                        <div className="hex-home nineteen-home"></div>
+                        <div className="hex-home twenty-home link-home link-five-home"><p>Contact</p></div>
                     </div>
                 </div>
             </div>
