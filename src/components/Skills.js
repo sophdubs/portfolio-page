f00a377f7b2f5d6c0c90f3787110974cd074bcb5
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-02-14 15:21:27 -0800

    finished skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
+import '../stylesheets/Skills.css';
+
 class Skills extends React.Component {
     render() {
         return (
             <div className="main-container">
-                <div className="left-container"></div>
-                <div className="right-container"></div>
+                <div id="skills" className="skills">
+                    <div className="top-skills">
+                        <div className="list-of-skills">
+                            <p className="ul-skills">{`<ul class="skills">`}</p>
+                            <p>{`<li>Javascript</li>`}</p>
+                            <p>{`<li>HTML</li>`}</p>
+                            <p>{`<li>CSS</li>`}</p>
+                            <p>{`<li>React</li>`}</p>
+                            <p>{`<li>Node</li>`}</p>
+                            <p>{`<li>JQuery</li>`}</p>
+                            <p>{`<li>Git</li>`}</p>
+                            <p className="ul-skills">{`</ul>`}</p>
+                        </div>
+                    </div>
+                    <div className="bottom-skills">
+                        <h1 className="title-skills">My Skills</h1>
+                    </div>
+                </div>
+             
             </div>
         );
     };
--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import ReactDOM from 'react-dom';
 // import Home from './components/Home';
 // import AboutMe from './components/AboutMe';
 // import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
 import Skills from "./components/Skills";
 // import Blog from './components/Blog';
 
 import './default.css';
 
 
-ReactDOM.render(<Contact/>, document.getElementById('root'));
+ReactDOM.render(<Skills/>, document.getElementById('root'));
 
